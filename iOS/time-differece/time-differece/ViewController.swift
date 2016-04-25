729da51ed867e8b2589cde3c30a3fb1ebaf61518
saranpol <saran@hlpth.com>
2016-04-25 18:49:32 +0700

    asdf

---
+++ iOS/time-differece/time-differece/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  time-differece
+//
+//  Created by saranpol on 4/25/2559 BE.
+//  Copyright © 2559 hlp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
